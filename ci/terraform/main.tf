2016f8ac2ff2357585c3f8c78739db2d8a26541c
Aidan Feldman <aidan.feldman@gmail.com>
2021-01-25 03:49:16 -0500

    CI: automatically purge old content

--- ci/terraform/main.tf
+++ ci/terraform/main.tf
@@ -19,6 +19,27 @@ resource "azurerm_storage_account" "storeacc" {
   }
 }
 
+resource "azurerm_storage_management_policy" "prs" {
+  storage_account_id = azurerm_storage_account.storeacc.id
+
+  # automatically purge old content
+  # https://docs.microsoft.com/en-us/azure/storage/blobs/storage-lifecycle-management-concepts?tabs=azure-portal#expire-data-based-on-age
+  rule {
+    name    = "expirationRule"
+    enabled = true
+
+    filters {
+      blob_types = ["blockBlob"]
+    }
+
+    actions {
+      base_blob {
+        delete_after_days_since_modification_greater_than = 14
+      }
+    }
+  }
+}
+
 output "container" {
   value = azurerm_storage_account.storeacc.primary_web_host
 }
